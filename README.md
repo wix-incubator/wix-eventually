6a4501f26810822c1702e2d7c4178b288e898ea5
Vilius Lukosius <vilius@wix.com>
2017-12-15 11:09:38 +0200

    Import from internal module

---
+++ README.md
@@ -0,0 +1,53 @@
+# wix-eventually
+
+Retries promise until timeout expires. Use in your tests!
+
+Uses defaults:
+ - timeout: 10s;
+ - interval: 200ms;
+
+## install
+
+```bash
+npm install --save-dev wix-eventually
+```
+
+## usage
+
+```js
+const eventually = require('wix-eventually')
+const { expect } = require('chai')
+
+describe('example', () => {
+
+  it('should retry', () => {
+    let hasCompleted = false;
+  	setTimeout(() => hasCompleted = true, 1000)
+
+  	return Promise.resolve()  	  
+  	  .then(() => eventually(() => expect(hasCompleted).to.equal(true))
+  })
+})
+```
+
+## Api
+
+### (fn, opts): Promise
+Wraps a promise that retries a function n times with 200 ms delay in between;
+
+Arguments:
+ - fn - sync function or thenable.
+ - opts - optional object with:
+   - timeout - timeout for retrying, ms;
+   - interval - retry interval, ms.
+
+### with(defaults): (fn, opts)
+Returns a function with provided `defaults` - {timeout, interval}.
+
+
+## Note regarding testing
+
+Using this library might cause an anti-pattern of writing tests that take a long time to run.
+For example, if the code is using `setTimeout`, it's "easy" to use `eventually` to check the result.
+In case the timeout is set for 3 seconds, the test will now take 3 seconds.
+Instead, the code should be written in such way that the `setTimeout` functionality can be modified in the test to either run synchronously or with a very short timeout value.
---
+++ test/wix-eventually.spec.js
@@ -0,0 +1,47 @@
+const retry = require('..')
+const {expect} = require('chai')
+
+describe('wix-eventually', function() {
+  this.timeout(20000)
+
+  it('should retry 10 times with 1s delay by default and fail', done => {
+    const before = Date.now()
+
+    retry(() => { throw new Error('nope') }).catch(e => {
+      expect(e.message).to.be.string('Timeout of 10000 ms with: nope')
+      expect(Date.now() - before).to.be.gt(9000).and.be.lt(11000)
+      done()
+    })
+  })
+
+  it('should allow to set custom timeout', done => {
+    const before = Date.now()
+
+    retry(() => { throw new Error('nope') }, {timeout: 2000}).catch(e => {
+      expect(e.message).to.be.string('Timeout of 2000 ms with: nope')
+      expect(Date.now() - before).to.be.gt(1500).and.be.lt(2500)
+      done()
+    })
+  })
+
+  it('should pass-through succeeding promise', () => {
+    return retry(() => Promise.resolve())
+  })
+
+  it('should pass-through succeeding function', () => {
+    return retry(() => 'ok')
+  })
+
+  it('should eventually succeed', () => {
+    let okToProceed = false
+    setTimeout(() => okToProceed = true, 2000)
+    return retry(() => okToProceed === true ? Promise.resolve() : Promise.reject(new Error('nope')))
+  })
+
+  it('should be rejected with underlying error after timeout', done => {
+    retry(() => { throw new Error('nope') }, {timeout: 2000}).catch(e => {
+      expect(e.message).to.be.string('Timeout of 2000 ms with: nope')
+      done()
+    })
+  })
+})
