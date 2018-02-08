b0a84cb194eafa8f0b3f0fd0cbd8a16debcdc78b
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-02-08 19:44:30 +0200

    Update typings tests; add support for 'with'

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,10 @@ declare function WixEventually(fn: Function, opts?: WixEventually.Opts): Promise
 
 declare namespace WixEventually {
   export interface Opts {
-    timeout: number;
-    interval: number;
+    timeout?: number;
+    interval?: number;
   }
+
+  function _with(overrides: Opts): (fn: Function, opts?: WixEventually.Opts) => Promise<void>;
+  export { _with as with }
 }
--- test/typings.spec.ts
+++ test/typings.spec.ts
@@ -3,14 +3,61 @@ import * as eventually from '..';
 
 describe('wix-eventually types', () => {
   it('exports a function', () => {
-    expect(eventually).to.not.be.undefined;
     expect(eventually).to.be.a('function');
+  });
+
+  it('should return a promise', () => {
+    expect(eventually(() => true)).to.be.a('promise');
+  });
+
+  it('does not require options', () => {
+    eventually(() => true);
   });
 
   it('should accepts options', () => {
     eventually(() => true, {
       timeout: 1000,
       interval: 5000
+    });
+  });
+
+  it('should accepts partial options', () => {
+    eventually(() => true, {
+      timeout: 1000
+    });
+
+    eventually(() => true, {
+      interval: 5000
+    });
+  });
+
+  describe('with', () => {
+    it('should be a function', () => {
+      expect(eventually.with).to.be.a('function');
+    });
+
+    it('should require options', () => {
+      eventually.with({
+        timeout: 1000,
+        interval: 5000
+      });
+    });
+
+    it('should support partial options', () => {
+      eventually.with({});
+
+      eventually.with({
+        timeout: 1000
+      });
+
+      eventually.with({
+        interval: 5000
+      });
+    });
+
+    it('should return a function that returns a promise', () => {
+      expect(eventually.with({})).to.be.a('function');
+      expect(eventually.with({})(() => true)).to.be.a('promise');
     });
   });
 });
