2c0e0939202972bb86cca2461edd34320baa6590
Roman Shevchuk <romansh@wix.com>
2020-04-16 14:26:31 +0300

    pass types from function return value to eventually return value

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 export = WixEventually;
 
-declare function WixEventually(fn: Function, opts?: WixEventually.Opts): Promise<void>;
+type UnPromisify<T> = T extends Promise<infer U> ? U : T;
+
+declare function WixEventually<ReturnValue extends any>(fn: (...args: any) => ReturnValue, opts?: WixEventually.Opts): Promise<UnPromisify<ReturnValue>>;
 
 declare namespace WixEventually {
   export interface Opts {
@@ -8,6 +10,6 @@ declare namespace WixEventually {
     interval?: number;
   }
 
-  function _with(overrides: Opts): (fn: Function, opts?: WixEventually.Opts) => Promise<void>;
+  function _with(overrides: Opts): <ReturnValue extends any>(fn: (...args: any) => ReturnValue, opts?: WixEventually.Opts) => Promise<UnPromisify<ReturnValue>>;
   export { _with as with }
 }
